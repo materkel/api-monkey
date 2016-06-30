3dc87a10126607e2d3d0a0089b70af1c45d979c0
Matthias Freßdorf <mfressdorf.mmt-m2014@fh-salzburg.ac.at>
2016-06-30 18:24:56 +0200

    Use babel to support earlier node versions; Resolves #4

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 'use strict';
 
 // API Monkey Middleware
-module.exports = () => {
-  return function(req, res, next){
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+module.exports = function () {
+  return function (req, res, next) {
     /**
      * Set all monkey headers on the request object
      * for easy access and use in follow up request
      */
-    const monkeyHeaders = {};
-    Object.keys(req.headers).forEach(header => {
+    var monkeyHeaders = {};
+    Object.keys(req.headers).forEach(function (header) {
       if (header.split('_')[0] === 'monkey') {
         monkeyHeaders[header] = req.get(header);
       }
@@ -20,19 +25,21 @@ module.exports = () => {
      * handled route and initiate delay / error
      * accordingly
      */
-    const path = req.path.split('/').filter(x => x !== '');
-    const id = ['monkey', req.method].concat(path);
-    let header = null;
+    var path = req.path.split('/').filter(function (x) {
+      return x !== '';
+    });
+    var id = ['monkey', req.method].concat(path);
+    var header = null;
 
-    for (let monkeyHeader in monkeyHeaders) {
-      const headerTokens = monkeyHeader.split('_');
-      let match = true;
-      for (let i = 0; i < headerTokens.length; i++) {
+    for (var monkeyHeader in monkeyHeaders) {
+      var headerTokens = monkeyHeader.split('_');
+      var match = true;
+      for (var i = 0; i < headerTokens.length; i++) {
         if (id[i]) {
           if (headerTokens[i] === '*') {
             // If the monkey header ends with a "*", make sure the
             // current route ends too, else don't match
-            if ((i === headerTokens.length - 1) && (id[++i] !== undefined)) {
+            if (i === headerTokens.length - 1 && id[++i] !== undefined) {
               match = false;
               break;
             }
@@ -55,26 +62,42 @@ module.exports = () => {
     }
 
     if (header) {
-      let [ delay, error ] = header.split('/');
+      var _ret = function () {
+        var _header$split = header.split('/');
 
-      switch (error) {
-        case 'true':
-          error = 500;
-          break;
-        case 'none':
-          error = null;
-          break;
-        default:
-          // set custom error if specified
-          error = parseInt(error);
-      }
-      delay = (delay !== 'none') ? delay : 0;
+        var _header$split2 = _slicedToArray(_header$split, 2);
 
-      if (error !== null) {
-        return setTimeout(() => res.status(error).end(), delay);
-      }
-      return setTimeout(next, delay);
+        var delay = _header$split2[0];
+        var error = _header$split2[1];
+
+
+        switch (error) {
+          case 'true':
+            error = 500;
+            break;
+          case 'none':
+            error = null;
+            break;
+          default:
+            // set custom error if specified
+            error = parseInt(error);
+        }
+        delay = delay !== 'none' ? delay : 0;
+
+        if (error !== null) {
+          return {
+            v: setTimeout(function () {
+              return res.status(error).end();
+            }, delay)
+          };
+        }
+        return {
+          v: setTimeout(next, delay)
+        };
+      }();
+
+      if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
     }
     next();
   };
-}
+};
---
+++ src/index.es6
@@ -0,0 +1,80 @@
+'use strict';
+
+// API Monkey Middleware
+module.exports = () => {
+  return function(req, res, next){
+    /**
+     * Set all monkey headers on the request object
+     * for easy access and use in follow up request
+     */
+    const monkeyHeaders = {};
+    Object.keys(req.headers).forEach(header => {
+      if (header.split('_')[0] === 'monkey') {
+        monkeyHeaders[header] = req.get(header);
+      }
+    });
+    req.monkeyHeaders = monkeyHeaders;
+
+    /*
+     * Check if a header is directed at the current
+     * handled route and initiate delay / error
+     * accordingly
+     */
+    const path = req.path.split('/').filter(x => x !== '');
+    const id = ['monkey', req.method].concat(path);
+    let header = null;
+
+    for (let monkeyHeader in monkeyHeaders) {
+      const headerTokens = monkeyHeader.split('_');
+      let match = true;
+      for (let i = 0; i < headerTokens.length; i++) {
+        if (id[i]) {
+          if (headerTokens[i] === '*') {
+            // If the monkey header ends with a "*", make sure the
+            // current route ends too, else don't match
+            if ((i === headerTokens.length - 1) && (id[++i] !== undefined)) {
+              match = false;
+              break;
+            }
+            continue;
+          }
+          if (headerTokens[i] === '**') {
+            break;
+          }
+          if (id[i].toLowerCase() === headerTokens[i].toLowerCase()) {
+            continue;
+          }
+        }
+        match = false;
+        break;
+      }
+      if (match === true) {
+        header = monkeyHeaders[monkeyHeader];
+        break;
+      }
+    }
+
+    if (header) {
+      let [ delay, error ] = header.split('/');
+
+      switch (error) {
+        case 'true':
+          error = 500;
+          break;
+        case 'none':
+          error = null;
+          break;
+        default:
+          // set custom error if specified
+          error = parseInt(error);
+      }
+      delay = (delay !== 'none') ? delay : 0;
+
+      if (error !== null) {
+        return setTimeout(() => res.status(error).end(), delay);
+      }
+      return setTimeout(next, delay);
+    }
+    next();
+  };
+}
